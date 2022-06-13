a229f67967daf3037265895f9545779c9f99db28
Carlos M <carlosedm@protonmail.com>
2022-06-13 02:42:36 -0600

    Add alacritty, fish, i3, polybar, picom, rofi

---
+++ .config/fish/conf.d/personal.fish
@@ -0,0 +1,7 @@
+set fish_greeting
+fish_vi_key_bindings
+alias raspissh="ssh pi@192.168.0.93"
+alias raspisftp="sftp pi@192.168.0.93"
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias ls="ls -lh"
+neofetch
