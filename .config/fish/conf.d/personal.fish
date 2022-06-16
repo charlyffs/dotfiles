a030f522ff27b72da376fe3f944ea9615c79d118
Carlos M <carlosedm@protonmail.com>
2022-06-16 09:02:51 -0600

    Added battery to polybar, touchpad settings, etc.

--- .config/fish/conf.d/personal.fish
+++ .config/fish/conf.d/personal.fish
@@ -3,5 +3,6 @@ fish_vi_key_bindings
 alias raspissh="ssh pi@192.168.0.93"
 alias raspisftp="sftp pi@192.168.0.93"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias wifi='nmtui'
 alias ls="ls -lh"
 neofetch
