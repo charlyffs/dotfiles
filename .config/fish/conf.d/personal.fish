da44eb042f227a38514b5154b33db8a9f2189862
Carlos M <carlosedm@protonmail.com>
2022-06-17 22:07:52 -0600

    Added media and sink control shortcuts

--- .config/fish/conf.d/personal.fish
+++ .config/fish/conf.d/personal.fish
@@ -4,4 +4,5 @@ alias raspissh="ssh pi@192.168.0.93"
 alias raspisftp="sftp pi@192.168.0.93"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias ls="ls -lh"
+alias media="cd /run/media/charly"
 neofetch
