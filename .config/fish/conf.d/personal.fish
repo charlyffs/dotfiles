665f774882950b0ee76df4e6cad9ddce02470af8
Carlos M <carlosedm@protonmail.com>
2022-07-04 18:13:07 -0600

    Update Battery label

--- .config/fish/conf.d/personal.fish
+++ .config/fish/conf.d/personal.fish
@@ -1,5 +1,7 @@
 set fish_greeting
 fish_vi_key_bindings
+alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+alias orphans="sudo pacman -Qtdq | pacman -Rns -"
 alias raspissh="ssh pi@192.168.1.93"
 alias raspisftp="sftp pi@192.168.1.93"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
