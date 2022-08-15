3e21eb8d5619e38311db0acf536d7245db17869d
Carlos M <carlosedm@protonmail.com>
2022-08-14 17:32:29 -0700

    Misc

--- .config/fish/conf.d/personal.fish
+++ .config/fish/conf.d/personal.fish
@@ -1,11 +1,11 @@
 set fish_greeting
 fish_vi_key_bindings
-alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
-alias orphans="sudo pacman -Qtdq | pacman -Rns -"
 alias raspissh="ssh pi@192.168.1.93"
 alias raspisftp="sftp pi@192.168.1.93"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-alias wifi='nmtui'
+alias wifi="nmtui"
 alias ls="ls -lh"
 alias media="cd /run/media/charly"
+alias suspend="systemctl suspend"
+alias mac="ssh carlos.montano@192.168.1.151"
 neofetch
