58f5f39cf20e3c9f097155d374e93e25a0f33896
Carlos M <carlosedm@protonmail.com>
2022-08-24 09:56:24 -0700

    Misc

--- .config/fish/conf.d/personal.fish
+++ .config/fish/conf.d/personal.fish
@@ -1,7 +1,7 @@
 set fish_greeting
 fish_vi_key_bindings
-alias raspissh="ssh pi@192.168.1.93"
-alias raspisftp="sftp pi@192.168.1.93"
+alias raspissh="ssh pi@192.168.100.93"
+alias raspisftp="sftp pi@192.168.100.93"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias wifi="nmtui"
 alias ls="ls -lh"
